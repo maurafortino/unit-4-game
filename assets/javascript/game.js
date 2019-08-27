1612976864ee155fd3a8fb7acdc42f2557f2ee02
maurafortino <maurafortino@gmail.com>
2019-08-27 11:51:46 -0400

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-//list of global variables
-var magicNumText = $("#magic-number"); //gives access to magic-number id
-var magicNumber = randomNum(19, 120); //variable that holds the random number function 
-var winsText = $("#wins"); //gives access to the wins id
-var wins = 0; //stores the wins and will be updated when user wins
-var lossesText = $("#losses"); //gives access to the losses id
-var losses = 0;// stores the losses and will be updated when user loses
-var userNumberText = $("#user-number"); //gives access to the user-number h3
-var userNumber = 0; //this will store the user number
-var randomNumArray = [Math.floor(Math.random()*12 + 1), Math.floor(Math.random()*12 + 1), Math.floor(Math.random()*12 + 1), Math.floor(Math.random()*12 + 1)]
-//generates code for the magic number and then adds it to the HTML
-//code to display wins and losses text in the very beginning of game wins and losses should be 0
-winsText.text(wins);
-lossesText.text(losses);
+    //list of global variables
+    var magicNumText = $("#magic-number"); //gives access to magic-number id
+    var magicNumber, userNumber, randomNumArray;
+    var winsText = $("#wins"); //gives access to the wins id
+    var userNumberText = $("#user-number"); //gives access to the user-number h3
+    var wins = 0; //stores the wins and will be updated when user wins
+    var lossesText = $("#losses"); //gives access to the losses id
+    var losses = 0;// stores the losses and will be updated when user loses
 
-function randomNum(min, max){
-    return Math.floor(Math.random()*(max-min+1) + min);
-};
-magicNumText.text(magicNumber);
+    replay();
+    //uses a function to reset the game: includes new numbers for the crystals and new magic number; wins/losses will not be affected by this
+    function replay() {
+        winsText.text(wins);
+        lossesText.text(losses);
 
+        magicNumber = randomNum(19, 120); //stores the magic number from randomNum function below
+        magicNumText.text(magicNumber); //adds value stores in magicNumber var to HTML
+        userNumber = 0; //sets user number to zero
+        userNumberText.empty(); //clears user number div
+        $("#crystal-images").empty(); //clears crystal images
+        randomNumArray = [Math.floor(Math.random() * 12 + 1), Math.floor(Math.random() * 12 + 1), Math.floor(Math.random() * 12 + 1), Math.floor(Math.random() * 12 + 1)]; //creates an array used in for loop below
 
-//creates four crystals with a different value
+        //adds new crystals to the crystal-images div and assigns them new values based on the random number and position of index
+        for (var i = 0; i < randomNumArray.length; i++) {
+            var crystalImages = $("<img>");
 
-for(var i = 0; i < randomNumArray.length; i++){
-    var crystalImages = $("<img>");
+            crystalImages.addClass("crystal");
+            crystalImages.attr("src", "assets/images/white-crystal.png");
+            crystalImages.attr("value", randomNumArray[i]);
 
-    crystalImages.addClass("crystal");
-    crystalImages.attr("src", "assets/images/white-crystal.png");
-    crystalImages.attr("value", randomNumArray[i]);
+            $("#crystal-images").append(crystalImages);
+        }
 
+        //adds an onclick event to the crystal images to make them act as buttons
+        $(".crystal").on("click", function () {
+            var crystalNum = parseInt($(this).attr("value"));
+            userNumber += crystalNum
+            userNumberText.text(userNumber);
+            if (userNumber === magicNumber) {
+                wins++;
+                replay();
+            } else if (userNumber > magicNumber) {
+                losses++;
+                replay();
+            }
+        });
 
-    $("#crystal-images").append(crystalImages);
-    
     }
+    //generates code for the magic number
+    function randomNum(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
 
-$(".crystal").on("click", function(){
-    var crystalNum = parseInt($(this).attr("value"));
-    userNumber += crystalNum
-    userNumberText.text(userNumber);
-});
-
-
-if(userNumber === magicNumber){
-    console.log(wins++);
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//don't delete this - for the document ready function
 });
